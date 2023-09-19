8a7c83d2254760b49bdac56bce1a1095ae3a849b
lanja <ericvalisoa1998@gmail.com>
2023-09-19 14:16:03 +0300

    second commit

--- main.js
+++ main.js
@@ -2,7 +2,24 @@ import './style.css'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import { gsap } from 'gsap'
 
-gsap.from(".test", {
+const test = document.querySelector(".test")
+let mySplit = test.textContent
+test.textContent = ""
+let split = mySplit.split("")
+for (let i = 0; i < split.length; ++i) {
+  const mySpan = document.createElement("span")
+  mySpan.textContent = split[i]
+  test.appendChild(mySpan)
+
+}
+gsap.from(".test span", {
+  yPercent: 130,
+  stagger: 0.5,
+  duration: 2,
+  opacity: 0,
+})
+
+/*gsap.from(".h", {
   yPercent: 130,
   stagger: 0.05,
   duration: 3,
@@ -13,6 +30,6 @@ gsap.from(".test2", {
   stagger: 0.05,
   duration: 3,
   opacity: 0,
-});
+});*/
 
 
